ad6821e720cd6d8a44d94a09b3dcf33b75ba52f1
Mahmoud Abdulrhman <mahmoudtheeb@gmail.com>
2021-06-22 23:34:16 -0700

    fix syntax error in the controller.update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,11 +28,11 @@ class ArticlesController < ApplicationController
   def update 
     @article = Article.find(params[:id])
 
-    if @article.update.(article_params)
+    if @article.update(article_params)
       redirect_to @article
     end
   end
-  
+
 
   private
     def article_params
